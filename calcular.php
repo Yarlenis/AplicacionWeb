d22474c162f66716d14536dd811d8c6a699a7ec2
Yarlenis Alabarca <Yarlenis.alabarca@utp.ac.pa>
2021-07-14 23:24:55 +0200

    se ha modificado el archivo calcular.php

--- calcular.php
+++ calcular.php
@@ -11,3 +11,17 @@ switch($edad)
         echo "El doble de tu edad";
         break;
 }
+
+$edad = 24;
+switch($edad)
+{
+    case 12:
+        echo "La mitad de tu edad";
+        break;
+    case 24:
+        echo "Esta es tu edad";
+        break;
+    case 48:
+        echo "El doble de tu edad";
+        break;
+}
